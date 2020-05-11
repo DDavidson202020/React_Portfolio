04f9c95cb79a04e2997d8de5fd4dcdee08c17fd4
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-11 19:53:19 -0400

    fix small bug

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" components={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
         <Footer />
