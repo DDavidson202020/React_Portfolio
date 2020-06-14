8b64985720d604f6f2cf651deb59007b4ad0145e
Yoom Davidson <ddavidson202020@gmail.com>
2020-06-13 23:10:37 -0400

    Add UCF badge

--- src/NavTabs.js
+++ src/NavTabs.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-
+import badge from "./components/images/Badge.png";
 function NavTabs() {
 
   const location = useLocation();
 
   return (
     <div className="navbar navbar-expand-lg">
-      <h2 style={{marginLeft: 20}} id={"name"}>Dung Davidson</h2>
+      <h2 style={{marginLeft: 20}} id="name">Dung Davidson</h2>
+      <a href="https://badgr.com/public/assertions/xG2y5KuLSJ2g8EMQLFfPsg" target="_blank" rel="noopener noreferrer" style={{marginLeft: "35%"}}><img src={badge} alt="badge" height="60" width="60" /></a>
       <ul className="nav navbar-brand collapse navbar navbar-collapse">
       
         <li className="nav-item">
